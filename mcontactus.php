c820c24ce449627fb78c9fdfee3199da4330d0c0
Bondili Rohan Singh <operations.vellore@makeadiff.in>
2014-05-02 10:36:40 +0530

    [Added] Scheme Description & Contact Us page

---
+++ mcontactus.php
@@ -0,0 +1,186 @@
+<!DOCTYPE html>
+<head>
+<title>MNREGA|Contact Us</title>
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<link rel="shortcut icon" href="assets/icons/logo.ico">
+<script type="text/javascript">
+
+<!--
+var image1=new Image()
+image1.src="1.jpg"
+var image2=new Image()
+image2.src="2.jpg"
+var image3=new Image()
+image3.src="3.jpg"
+var image4=new Image()
+image4.src="4.jpg"
+var image5=new Image()
+image5.src="5.jpg"
+var image6=new Image()
+image6.src="6.jpg"
+//-->
+</script>
+<style>
+    body
+    {
+    background-color: rgba(155, 188, 147, 0.33);
+    font-family: 'Roboto Condensed', sans-serif;
+    }
+    #imgone
+{
+position: fixed;
+bottom:30px;
+}
+#imgtwo
+{
+position: fixed;
+right:5%;
+height:40px;
+    width:300px;
+bottom:30px;
+background-color:"#a59292";
+
+}
+    #one
+    {
+        background-color: ;
+        text-align: ;
+        margin-left: 200px;
+        margin-right: 200px;
+        height:100px;
+        font-size:30pt;
+    /*    font-family: "copper black";
+    */
+    }
+    #two
+    {
+        background-color:#0099CC;
+        margin-left: 200px;
+        margin-right: 195px;
+        height:50px;
+    }
+    a
+    {
+        word-spacing: 5px;
+        margin-left:10px;
+        font-size:15pt;
+        text-decoration: none;
+        color:white;
+    }
+    #p
+    {
+        word-spacing:15px; 
+        letter-spacing: 1px;
+        padding-left:150px;
+        padding-top:10px;
+        padding-right: 150px;
+    }
+    img
+    {
+        border:2px white solid;
+        width:950px;
+        height:300px;
+    }
+    #three
+    {
+        margin-left: 200px;
+        margin-right: 200px;
+    }
+    #four
+    {
+        height:150px;
+        margin-left: 200px;
+        margin-right: 200px;
+        background-color: ;
+        
+    }
+    #five
+    {
+        padding-top:2px;
+        height:550px;
+        width:300px;
+        text-align: right;
+        border-right: 2px solid black;
+    /*    font-family: "comic sans MS";
+    */    font-size: 20pt;
+        float:left;
+    }
+    #six
+    {
+    /*    font-family: "Times New Roman";
+    */   padding-top: 0px;
+       font-size:15pt;
+        text-align: left;
+        margin-left:320px;
+    }
+    #sev
+    {
+        margin-left: 200px;
+        margin-right: 200px;
+    }
+    #i
+    {
+        height:30px;
+    }
+    a:hover {height:50px;
+        text-decoration: none;}
+    </style>
+</head>
+
+<body>
+<div id="one" ><center>MNREGA</center></div>
+    <div id="two">
+    <p id="p">
+    <a href="mindex.php" "active">Home</a>
+    <a href="mscheme.php">Scheme</a>
+    <a href="mregistration.php">Register</a>
+    <a href="mlogin.php">Administrator login</a>
+    <a href="mcontactus.php">Contact Us</a>
+</p>
+    </div>
+    <img src="1.jpg" name="slide" width="900" height="450" style="margin-left:200px;" />
+<script>
+<!--
+//variable that will increment through the images
+var step=1
+function slideit(){
+//if browser does not support the image object, exit.
+if (!document.images)
+return
+document.images.slide.src=eval("image"+step+".src")
+if (step<6)
+step++
+else
+step=1
+//call function "slideit()" every 2.5 seconds
+setTimeout("slideit()",4000)
+}
+slideit()
+//-->
+    </script>
+    </br>
+    </br>
+    <div id="four">
+    <div id="five">Contact <br> Us <br></div>
+    <div id="six">&nbsp;&nbsp;&nbsp;You can contact us by filling the form below. We will                    
+reply to you as soon as possible over email.
+        <br><br><br>MNREGA-feedback form                  
+<br>Chanakya Puri,Hauz Khas village
+ <br>New Delhi, India
+ <br>Phone:   +91 413 222 7750
+ <br>Fax:    +91 413 2227 141
+ <br>Mail: mnrega@nic.in <br><br>
+        
+        <form method="post"><br>
+        Name:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="text"><br>
+        Email:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="email"><br>
+        Comment:&nbsp;<textarea cols="30" rows="2"></textarea>
+        <br><br><input type="submit" value="Submit"/ style="margin-left:80px;">
+        </form>
+        </div>
+        </div>
+    <div id="sev">
+    <img src="footer.png" id="i">
+    </div>
+</body>
+</html>
---
+++ mscheme.php
@@ -0,0 +1,170 @@
+<!DOCTYPE html>
+<head>
+<title>MNREGA|Scheme</title>
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<link rel="shortcut icon" href="assets/icons/logo.ico">
+<script type="text/javascript">
+<!--
+var image1=new Image()
+image1.src="1.jpg"
+var image2=new Image()
+image2.src="2.jpg"
+var image3=new Image()
+image3.src="3.jpg"
+var image4=new Image()
+image4.src="4.jpg"
+var image5=new Image()
+image5.src="5.jpg"
+var image6=new Image()
+image6.src="6.jpg"
+//-->
+</script>
+<style>
+    body
+    {
+    background-color: rgba(155, 188, 147, 0.33);
+    font-family: 'Roboto Condensed', sans-serif;
+    
+    }
+    #one
+    {
+        background-color: ;
+        text-align: ;
+        margin-left: 200px;
+        margin-right: 200px;
+        height:100px;
+        font-size:30pt;
+    
+    }
+    #two
+    {
+        background-color: #0099CC;
+        margin-left: 200px;
+        margin-right: 195px;
+        height:50px;
+    }
+    a
+    {
+        word-spacing: 5px;
+        margin-left:10px;
+        font-size:15pt;
+        text-decoration: none;
+        color:white;
+    }
+    #p
+    {
+        word-spacing:15px; 
+        letter-spacing: 1px;
+        padding-left:150px;
+        padding-top:10px;
+        padding-right: 150px;
+    }
+    img
+    {
+        border:2px white solid;
+        width:950px;
+        height:300px;
+    }
+    #three
+    {
+        margin-left: 200px;
+        margin-right: 200px;
+    }
+    #four
+    {
+        height:150px;
+        margin-left: 200px;
+        margin-right: 200px;
+        background-color: ;
+        
+    }
+    #five
+    {
+        padding-top:2px;
+        height:380px;
+        width:300px;
+        text-align: right;
+        /*font-family: "comic sans MS";
+        */border-right: 2px solid black;
+        font-size: 20pt;
+        float:left;
+    }
+    #six
+    {
+       /* font-family: "Times New Roman";
+       */padding-top: 0px;
+       font-size:18px;
+        text-align: left;
+        margin-left:320px;
+    }
+    #sev
+    {
+        margin-left: 200px;
+        margin-right: 200px;
+        position: relative;
+    }
+    #i
+    {
+        height:30px;
+    }
+    a:hover {text-decoration: none; 
+        height:50px;}
+    
+
+/*    a:active {background-color:#000000;}    
+*/    </style>
+</head>
+
+<body>
+<div id="one" ><center>MNREGA</center></div>
+    <div id="two">
+    <p id="p">
+    <a href="mindex.php" "active">Home</a>
+    <a href="mscheme.php">Scheme</a>
+    <a href="mregistration.php">Register</a>
+    <a href="mlogin.php">Administrator login</a>
+    <a href="mcontactus.php">Contact Us</a>
+    </p>
+    </div>
+    <img src="1.jpg" name="slide" width="900" height="450" style="margin-left:200px;" />
+<script>
+<!--
+//variable that will increment through the images
+var step=1
+function slideit(){
+//if browser does not support the image object, exit.
+if (!document.images)
+return
+document.images.slide.src=eval("image"+step+".src")
+if (step<6)
+step++
+else
+step=1
+//call function "slideit()" every 2.5 seconds
+setTimeout("slideit()",4000)
+}
+slideit()
+//-->
+    </script>
+    </br>
+    </br>
+    <div id="four">
+    <div id="five">Scheme <br></div>
+    <div id="six">&nbsp;&nbsp;&nbsp;The popular NREGA is designed at improving the 
+income protection of the people in rural areas by ensuring hundred days of salary 
+occupation in a financial year, to a rural family whose associates offer to do inexperienced
+guide work. The purpose of the Act is to make resilient resources and enhance the 
+income source platform of the rural inadequate. </p><p>The choice of works recommended in the
+ Act deal with causes of serious hardship such as famine. The high deforestation, 
+complete land problem, in order to  course of action of occupation creation is on a 
+maintainable basis works recommended in the Act details causes of serious hardship 
+like famine, deforestation and land break down completely, in order to  the modus 
+operandi of occupation creation is managed on a maintainable base. Therefore, by
+ observing the main aims and objectives of this government scheme, people can easily
+ understand the benefits of the program.</p></div>
+        </div>
+    <div id="sev">
+    <img src="footer.png" id="i">
+    </div>
+</body>
+</html>
